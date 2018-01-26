a8f20b7eb0504f04fdfa6153be4e3ac0c450ff68
Wil Morgan <wil.me.morgan@gmail.com>
2018-01-26 18:21:19 +0800

    Add ability to add items to the shopping list.

--- Section_11-DOM_Manipulation/Lecture_Materials/script.js
+++ Section_11-DOM_Manipulation/Lecture_Materials/script.js
@@ -1,5 +1,31 @@
-var button = document.getElementsByTagName("button")[0];
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
 
-button.addEventListener("mouseleave", function() {
-    console.log("CLICK!!!!");
-})
+function inputLength() {
+    return input.value.length;
+}
+
+function createListElement() {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+}
+
+function addListAfterClick() {
+    if (inputLength() > 0) {
+        createListElement();
+    }
+}
+
+function addListAfterKeypress(event) {
+    if (inputLength() > 0 && event.keyCode === 13) {
+        var li = document.createElement("li");
+        createListElement();
+    }
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
