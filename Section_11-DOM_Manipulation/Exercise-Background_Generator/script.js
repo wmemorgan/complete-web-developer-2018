582696663d2d4126adb5b80550416ab8d7c1415a
Wil Morgan <wil.me.morgan@gmail.com>
2018-01-29 17:09:19 +0800

    Add background generator feature. (Followed Section 11 Lecture 97 demo)

--- Section_11-DOM_Manipulation/Exercise-Background_Generator/script.js
+++ Section_11-DOM_Manipulation/Exercise-Background_Generator/script.js
@@ -4,14 +4,13 @@ var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
 function setGradient() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
+    body.style.background =
+        "linear-gradient(to right, "
+        + color1.value
+        + ", "
+        + color2.value + ")";
 
-	css.textContent = body.style.background + ";";
+    css.textContent = body.style.background + ";";
 }
 
 color1.addEventListener("input", setGradient);
