15bd4fcaf2f0361675395e92b23dd72793e90ae2
Wil Morgan <wil.me.morgan@gmail.com>
2018-01-30 12:09:35 +0800

    Add random color generator function.
    Add random background color generator function.

--- Section_11-DOM_Manipulation/Exercise-Background_Generator/script.js
+++ Section_11-DOM_Manipulation/Exercise-Background_Generator/script.js
@@ -2,6 +2,7 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var randomColor1 = document.querySelector(".randomColor");
 
 function setGradient() {
     body.style.background =
@@ -13,6 +14,30 @@ function setGradient() {
     css.textContent = body.style.background + ";";
 }
 
+function randomColorGenerator() {
+    var rgb = [];
+    for (n = 0; n < 3; n++) {
+        rgb.push(Math.floor(Math.random() * 256));
+    }
+    return 'rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')';
+
+}
+
+function randomBackgroundGenerator() {
+    body.style.background =
+        "linear-gradient(to right, "
+        + randomColorGenerator()
+        + ", "
+        + randomColorGenerator() + ")";
+
+    css.textContent = body.style.background + ";";
+}
+
+setGradient();
+
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+randomColor1.addEventListener("click", randomBackgroundGenerator);
+
